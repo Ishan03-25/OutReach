4dc812521133530d99493c55a8814f79371fb95c
Ishan <ishan03sharma25@gmail.com>
2026-03-06 02:01:07 +0530

    Multi email account outreact implemented

--- backend/src/Mail/send.ts
+++ backend/src/Mail/send.ts
@@ -3,24 +3,45 @@ import dotenv from "dotenv";
 import { getPublicUrl } from "../supaBase/bucket/getUrl";
 dotenv.config();
 
-const transporter = nodemailer.createTransport({
-  service: "gmail", 
-  auth: {
-    type: "OAuth2",
-    user: process.env.GOOGLE_EMAIL_1,
-    clientId: process.env.GOOGLE_CLIENT_ID_1,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET_1,
-    refreshToken: process.env.GOOGLE_REFRESH_TOKEN_1
-  },
+console.log("Environment variables loaded: ", {
+  GOOGLE_EMAIL_1: process.env.GOOGLE_EMAIL_1,
+  GOOGLE_CLIENT_ID_1: process.env.GOOGLE_CLIENT_ID_1,
+  GOOGLE_CLIENT_SECRET_1: process.env.GOOGLE_CLIENT_SECRET_1,
+  GOOGLE_REFRESH_TOKEN_1: process.env.GOOGLE_REFRESH_TOKEN_1,
 });
 
-async function sendMail(attachmentPath: string) {
+// const transporter = nodemailer.createTransport({
+//   service: "gmail", 
+//   auth: {
+//     type: "OAuth2",
+//     user: process.env.GOOGLE_EMAIL_1,
+//     clientId: process.env.GOOGLE_CLIENT_ID_1,
+//     clientSecret: process.env.GOOGLE_CLIENT_SECRET_1,
+//     refreshToken: process.env.GOOGLE_REFRESH_TOKEN_1
+//   },
+// });
+
+const createtransporter = (email: string, refreshToken: string) => {
+  return nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      type: "OAuth2",
+      user: email,
+      clientId: process.env.GOOGLE_CLIENT_ID_1,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET_1,
+      refreshToken: refreshToken
+    },
+  })
+}
+
+async function sendMail(attachmentPath: string, from: string, to: string, refreshToken: string) {
   try {
+    const transporter = createtransporter(from, refreshToken);
     const res = await transporter.sendMail({
-      from: process.env.GOOGLE_EMAIL_1,
-      to: "ishan03sharma25@gmail.com",
+      from: from,
+      to: to,
       subject: "nodemailer",
-      text: "Hello from Ishan, nodemailer",
+      text: `Hello, testing nodemailer from email id ${from} to email id ${to}`,
       attachments: [
         {
             filename: "Ishan_SDE.pdf",
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,13 +13,13 @@
 //   console.error("Error in Sending Mail:", error);
 // }
 
-import express from 'express';
-import { getGmailClient } from './Mail/client';
-import axios from 'axios';
-import nodeCron from 'node-cron';
-import dotenv from 'dotenv';
-import { getPublicUrl } from './supaBase/bucket/getUrl';
-import { sendMail } from './Mail/send';
+import express from "express";
+import { getGmailClient } from "./Mail/client";
+import axios from "axios";
+import nodeCron from "node-cron";
+import dotenv from "dotenv";
+import { getPublicUrl } from "./supaBase/bucket/getUrl";
+import { sendMail } from "./Mail/send";
 dotenv.config();
 // import { startWatchGmail } from './Mail/watch';
 
@@ -28,23 +28,73 @@ const port = 3000;
 
 app.use(express.json());
 
-nodeCron.schedule('0 58 22 * * 1-5', ()=>{
+nodeCron.schedule("0 49 01 * * 1-5", async () => {
   const filePath = process.env.SUPABASE_FILE_PATH;
-  if (!filePath){
+  if (!filePath) {
     throw new Error("File path not found in environment variables");
   }
   const cvurl = getPublicUrl(filePath);
   try {
-    sendMail(cvurl);
+    let emails_from: {email: string, refreshToken: string}[] = [];
+    for (let i = 1; i <= 5; i++) {
+      const email: string | undefined = process.env[`GOOGLE_EMAIL_${i}`];
+      const refreshToken: string | undefined = process.env[`GOOGLE_REFRESH_TOKEN_${i}`];
+      if (!email) {
+        console.log(
+          `GOOGLE_EMAIL_${i} not found in environment variables in emails from loop`,
+        );
+        continue;
+      }
+      if (!refreshToken){
+        console.log(
+          `GOOGLE_REFRESH_TOKEN_${i} not found in environment variables in emails from loop`,
+        );
+        continue;
+      }
+      emails_from.push({email, refreshToken});
+    }
+    console.log("Emails from which mails will be sent: ", emails_from);
+    let emails_to: string[] = [];
+    for (let i = 0; i < emails_from.length; i++) {
+      for (let j = emails_from.length - 1; j >= 0; j--) {
+        if (!emails_from[i].email) {
+          console.log(
+            `GOOGLE_EMAIL_${i + 1} not found in environment variables in emails_from nested loops`,
+          );
+          continue;
+        }
+        if (!emails_from[j].refreshToken){
+          console.log(
+            `GOOGLE_REFRESH_TOKEN_${j + 1} not found in environment variables in emails_from nested loops`,
+          );
+          continue;
+        }
+        // if (!emails_to[j]){
+        //   console.log(`GOOGLE_EMAIL_${j+1} not found in environment variables in emails_to nested loops`);
+        //   continue;
+        // }
+        try {
+          const res = await sendMail(cvurl, emails_from[i].email, emails_from[j].email, emails_from[i].refreshToken);
+          console.log(
+            `Scheduled mail sent successfully from ${emails_from[i].email} to ${emails_from[j].email}`,
+          );
+        } catch (error) {
+          console.log(
+            `Error in sending scheduled mail from ${emails_from[i].email} to ${emails_from[j].email }: `,
+            error,
+          );
+        }
+      }
+    }
     console.log("Scheduled mail sent successfully");
   } catch (error) {
     console.log("Error in sending scheduled mail: ", error);
   }
-})
+});
 
-app.get('/', (req, res)=>{
+app.get("/", (req, res) => {
   res.send("Hello World!");
-})
+});
 
 // app.post("/gmail/webhook", (req, res)=>{
 //   try {
@@ -61,50 +111,56 @@ app.get('/', (req, res)=>{
 
 let lastHistoryId: string | null = null;
 
-app.post("/gmail/webhook", async (req, res)=>{
+app.post("/gmail/webhook", async (req, res) => {
   try {
     const message = req.body.message;
     const decoded = JSON.parse(Buffer.from(message.data, "base64").toString());
     const gmail = await getGmailClient();
-    if (!lastHistoryId){
+    if (!lastHistoryId) {
       lastHistoryId = decoded.historyId;
       console.log("Initial history id set: ", lastHistoryId);
       return res.sendStatus(200);
     }
     const history = await gmail.users.history.list({
       userId: "me",
-      startHistoryId: lastHistoryId
+      startHistoryId: lastHistoryId,
     });
     const changes = history.data.history || [];
-    for (const change of changes){
+    for (const change of changes) {
       if (!change.messagesAdded) continue;
       for (const msg of change.messagesAdded) {
         if (!msg.message?.id) continue;
         const email = await gmail.users.messages.get({
           userId: "me",
-          id: msg.message?.id
+          id: msg.message?.id,
         });
         const labels = email.data.labelIds || [];
         const isIncoming = labels.includes("INBOX") && !labels.includes("SENT");
         if (!isIncoming) continue;
-        const from = email.data.payload?.headers?.find(h=>h.name==="From")?.value || "";
+        const from =
+          email.data.payload?.headers?.find((h) => h.name === "From")?.value ||
+          "";
         if (from.includes(process.env.GOOGLE_EMAIL_1!)) continue;
         try {
-          const res = await axios.post(`https://graph.facebook.com/v25.0/${process.env.WHATSAPP_PHONE_NUMBER_ID}/messages`, {
-            messaging_product: "whatsapp",
-            recipient_type: "individual",
-            to: process.env.WHATSAPP_USER_PHONE_NUMBER,
-            type: "text",
-            text: {
-              preview_url: false,
-              body: `Hi Ishan, you have a new email from ${from} with subject: ${email.data.payload?.headers?.find(h=>h.name==="subject")?.value || "No Subject"}. Snippet: ${email.data.snippet}`
-            }
-          }, {
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${process.env.WHATSAPP_ACCESS_TOKEN}`
-            }
-          });
+          const res = await axios.post(
+            `https://graph.facebook.com/v25.0/${process.env.WHATSAPP_PHONE_NUMBER_ID}/messages`,
+            {
+              messaging_product: "whatsapp",
+              recipient_type: "individual",
+              to: process.env.WHATSAPP_USER_PHONE_NUMBER,
+              type: "text",
+              text: {
+                preview_url: false,
+                body: `Hi Ishan, you have a new email from ${from} with subject: ${email.data.payload?.headers?.find((h) => h.name === "subject")?.value || "No Subject"}. Snippet: ${email.data.snippet}`,
+              },
+            },
+            {
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${process.env.WHATSAPP_ACCESS_TOKEN}`,
+              },
+            },
+          );
           console.log("whatsapp alert sent successfully: ", res);
         } catch (error) {
           console.log("Error in sedning whatsapp alert of new mail: ", error);
@@ -112,14 +168,14 @@ app.post("/gmail/webhook", async (req, res)=>{
         console.log("New email recieved: ", email.data.snippet);
       }
     }
-    lastHistoryId = decoded.historyId
+    lastHistoryId = decoded.historyId;
     return res.sendStatus(200);
-  } catch (error){
+  } catch (error) {
     console.log("error in gmail webhook: ", error);
     return res.sendStatus(500);
   }
-})
+});
 
-app.listen(port, ()=>{
+app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-})
+});
